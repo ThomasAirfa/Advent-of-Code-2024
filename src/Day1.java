70e020e2c22037a9e26158f5a91bb51790f87e74
ThomasAirfa <thomasguillotin@free.fr>
2025-06-17 23:25:28 +0000

    Day 1 part 2

--- src/Day1.java
+++ src/Day1.java
@@ -1,7 +1,11 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 static final Pattern SPACES = Pattern.compile(" +");
@@ -25,13 +29,22 @@ int distanceSum(int[] lefts, int[] rights) {
   return IntStream.range(0, lefts.length).map(i -> distance(lefts[i], rights[i])).sum();
 }
 
+long similarityScore(int[] lefts, int[] rights) {
+  Map<Integer, Long> rightsOccurenceMap = Arrays.stream(rights).boxed()
+      .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+  return Arrays.stream(lefts).map(i -> i * rightsOccurenceMap.getOrDefault(i, 0L).intValue()).sum();
+}
+
 void main(String... args) {
   var path = Path.of("input", "Day1_input.txt");
   try (var reader = Files.newBufferedReader(path)) {
+    // PART 1
     var pairs = reader.lines().map(this::parseDistancePair).toList();
     var lefts = pairs.stream().mapToInt(DistancePair::left).sorted().toArray();
     var rights = pairs.stream().mapToInt(DistancePair::right).sorted().toArray();
     System.out.println(distanceSum(lefts, rights));
+    // PART 2
+    System.out.println(similarityScore(lefts, rights));
   } catch (IOException e) {
     System.out.println(e.getMessage());
     System.exit(1);
